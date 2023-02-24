f65dc1a83b7de22a14a7fd08691b7d5b864518ce
Dustin <deeredman1991@gmail.com>
2023-02-23 19:31:50 -0600

    fixed keyboard input

--- src/main/scenes/game/entities/player/Player.gd
+++ src/main/scenes/game/entities/player/Player.gd
@@ -1,21 +1,35 @@
 extends KinematicBody2D
 
 
-export var speed:= 5
+var acceleration = 300
+var max_speed = 100
+var friction = 300
 
-func _physics_process(_delta: float) -> void:
+var velocity = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
 	if GameManager.game_is_paused:
 		return
 
-	handle_movement()
+	handle_movement(delta)
 
-func handle_movement() -> void:
-	var x := Input.get_axis("move_left", "move_right")
-	var y := Input.get_axis("move_up", "move_down")
+func handle_movement(delta: float) -> void:
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_axis("KB_move_left", "KB_move_right")
+	input_vector.y = Input.get_axis("KB_move_up", "KB_move_down")
 
-	var direction_vector = Vector2(x, y)
-	
-	if direction_vector.length() > 1:
-		direction_vector = direction_vector.normalized()
+	# We have to separate Joypad Controls from Keyboard Controls
+	#	because joypad will randomly interupt keyboard if plugged in.
+	if input_vector == Vector2.ZERO:
+		input_vector.x = Input.get_axis("JP_move_left", "JP_move_right")
+		input_vector.y = Input.get_axis("JP_move_up", "JP_move_down")
 
-	move_and_slide( direction_vector * speed * ((OptionsManager.tile_size.x + OptionsManager.tile_size.y)/2) )
+	if input_vector.length() > 1:
+		input_vector = input_vector.normalized()
+
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward( input_vector * max_speed, acceleration * delta )
+	else:
+		velocity = velocity.move_toward( Vector2.ZERO, friction * delta )
+
+	velocity = move_and_slide( velocity )
